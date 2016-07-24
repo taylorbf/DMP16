441d815770843092a368658fa3b5edefff2088f1
Ben Taylor <taylorbf@gmail.com>
2016-07-24 15:35:03 -0400

    Theramin improvements

--- class4/Theramin/sketch.js
+++ class4/Theramin/sketch.js
@@ -1,14 +1,18 @@
 function setup() {
-  createCanvas(windowWidth,windowHeight)
+  createCanvas(800,500)
   osc = new p5.Oscillator()
-  osc.setType('sine')
+  osc.setType('triangle')
   osc.start()
 }
 
 function draw() {
-  background(255)
-  osc.freq(mouseX,0.02)
-  osc.amp(mouseY/height,0.02)
+  background(255,100)
   
-  text("testing",50,50)
+  var volume = map(mouseY,height,0,0,0.6)
+  
+  osc.freq(mouseX,0.01)
+  osc.amp(volume,0.01)
+  
+  fill(0,100,255,80)
+  ellipse(mouseX,mouseY,40,40)
 }
